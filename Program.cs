44df21dba7f3646bb96a65ae41e7f0541d74a154
Alexandre Emygdio <emygdio.alex@gmail.com>
2023-11-08 21:07:56 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+             /*
+                quero que exiba a tabuada do numero digitado 
+                5 X 1 = 5 
+                5 X 2 = 10
+                5 X 3 = 15             
+             */
+
+            // sequenciador 
+            Console.WriteLine("Digite um numero");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = numero; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
